59095a48d4a2570ee727a5cf2f5ccc3468b82a2c
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-13 09:16:08 -0300

    finished darkmode logic

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ function wholeProgram() {
     "Nem sonhando",
   ];
   const randomArrayElement = array[Math.floor(array.length * Math.random())];
-
   const submitButton = document.querySelector(".submitButton");
   submitButton.addEventListener("click", () => {
     const selectInputText = document.querySelector(".stringBox");
@@ -20,3 +19,23 @@ function wholeProgram() {
   });
 }
 wholeProgram();
+
+function darkMode() {
+  const darkModeBg = document.querySelector(".dark-mode");
+  const darkModeBall = document.querySelector(".dark-mode-toggle");
+  darkModeBg.classList.add("active");
+  darkModeBall.classList.add("active");
+
+  if (darkModeBg) {
+    const newColorH1 = document.querySelector("h1");
+    const newColorP = document.querySelector("p");
+    const newColorNewText = document.querySelector(".randomText");
+    const newColorBody = document.querySelector("body");
+    const newColorStringBox = document.querySelector(".stringBox");
+    newColorH1.classList.add("active");
+    newColorP.classList.add("active");
+    newColorNewText.classList.add("active");
+    newColorBody.classList.add("active");
+    newColorStringBox.classList.add("active");
+  }
+}
