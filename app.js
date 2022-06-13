4bc52a10586850a8e3839f55e217a5540a9b6af4
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-13 03:54:18 -0300

    add logic to check if theres content in the box

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const array = [
-  "Sim",
-  "Não",
-  "Talvez",
-  "Com certeza!",
-  "Claro!",
-  "Nem pensando",
-  "Nem sonhando",
-];
-const randomArrayElement = array[Math.floor(array.length * Math.random())];
+function wholeProgram() {
+  const array = [
+    "Sim",
+    "Não",
+    "Talvez",
+    "Com certeza!",
+    "Claro!",
+    "Nem pensando",
+    "Nem sonhando",
+  ];
+  const randomArrayElement = array[Math.floor(array.length * Math.random())];
 
-const submitButton = document.querySelector(".submitButton");
-submitButton.addEventListener("click", () => {
-  document.querySelector(".randomText").innerHTML = `${randomArrayElement}`;
-});
+  const submitButton = document.querySelector(".submitButton");
+  submitButton.addEventListener("click", () => {
+    const selectInputText = document.querySelector(".stringBox");
+    if (selectInputText.value === "") {
+      return alert("Por favor, insira algo dentro da caixa de texto.");
+    }
+    document.querySelector(".randomText").innerHTML = `${randomArrayElement}`;
+  });
+}
+wholeProgram();
